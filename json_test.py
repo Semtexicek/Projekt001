c853b98426ee9c3bee9823e91762db5481ad0882
Semtexicek <40370698+Semtexicek@users.noreply.github.com>
2018-06-18 22:10:11 +0100

    test on

---
+++ json_test.py
@@ -0,0 +1,22 @@
+import json
+
+some_data = { "jmeno" : "lala",
+              "characterclass" : "warrior"
+         }
+
+# ulozi data do souboru ( 'w' - write )
+with open("json_test.json", 'w') as file:
+    json.dump(some_data, file)
+
+# nacte data ze souboru ( 'r' - read )
+with open("json_test.json", 'r') as file:
+    nacteny_data = json.load(file)
+
+print(nacteny_data["jmeno"])
+
+some_data["bla"] = "Fn"
+
+print(some_data)
+
+with open("json_test.json", 'w') as file:
+    json.dump(some_data, file)
