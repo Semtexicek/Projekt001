a8bf1d33b860bb43b5528fe2cd73000410b82e94
Sebastian Wallo <sebastian.wallo@outlook.com>
2018-06-19 22:56:59 +0200

    quests init

---
+++ questloader.py
@@ -0,0 +1,17 @@
+import json
+
+def load_quest(identificator):
+    data = open_json(identificator)
+
+    if is_done(data):
+        return -1
+
+    return data
+
+def open_json(identificator):
+    json_name = identificator + ".json"
+    with open(json_name, 'r') as file:
+        return json.load(file)
+
+def is_done(data):
+    return data["done"]
