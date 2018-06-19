7c4b0741d832412f57c4fedddd5d2e1be49a6124
Semtexicek <40370698+Semtexicek@users.noreply.github.com>
2018-06-19 18:23:29 +0100

    a

--- chapter1.py
+++ chapter1.py
@@ -3,9 +3,12 @@ import json
 
 with open("dchapter1check.json", 'r') as file:
     dchapter1check = json.load(file)
-
 with open("dchapter1.json", 'r') as file:
     dchapter1 = json.load(file)
 
+
 if dchapter1check["Prvni kecy"] == 0:
     print(dchapter1["Dialog1"])
+    dchapter1check["Prvni kecy"] = 1
+    with open("dchapter1check.json", 'w') as file:
+        json.dump(dchapter1check, file)
