0df4a3a2d787125066de7f732779b6e1f6102db5
Sebastian Wallo <sebastian.wallo@konicaminolta.cz>
2018-07-11 20:21:10 +0200

    removed that evil hook

--- messagehandler.py
+++ messagehandler.py
@@ -5,17 +5,19 @@ import json
 # url
 HOOKURL = "https://discordapp.com/api/webhooks/456596395927797771/Sc4Rv6p3Af8nDeH_ywV5uogdat3PU2NeUCuKNaPPON32FhgTk_aTpl9Cg02ejqId3ZeK"
 
-'''post given message'''
+# '''post given message'''
+# def post_message(message):
+#     header = {"Content-Type" : "application/json"}
+#     messagejson = json.dumps({"content" : message})
+    
+#     response = requests.post(HOOKURL, messagejson, headers = header)
+
+#     if response.ok:
+#         print ("OK!")
+#         return True
+    
+#     print(response.reason)
+#     return False
+
 def post_message(message):
-    header = {"Content-Type" : "application/json"}
-    messagejson = json.dumps({"content" : message})
-    
-    response = requests.post(HOOKURL, messagejson, headers = header)
-
-    if response.ok:
-        print ("OK!")
-        return True
-    
-    print(response.reason)
-    return False
-
+    print(message)
