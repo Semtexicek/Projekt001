a8f856b51b51763431b00ff1ce8f74d6a3adf445
Sebastian Wallo <sebastian.wallo@konicaminolta.cz>
2018-07-11 20:22:04 +0200

    removed hook second time

--- messagehandler.py
+++ messagehandler.py
@@ -3,7 +3,6 @@ import requests
 import json
 
 # url
-HOOKURL = "https://discordapp.com/api/webhooks/456596395927797771/Sc4Rv6p3Af8nDeH_ywV5uogdat3PU2NeUCuKNaPPON32FhgTk_aTpl9Cg02ejqId3ZeK"
 
 # '''post given message'''
 # def post_message(message):
